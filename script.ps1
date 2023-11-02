d15b5d8f7b1f1dd04da77f218c73b862cfb60f68
mtaisbad <135580721+mtaisbad@users.noreply.github.com>
2023-11-02 16:39:44 +0100

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,15 @@
+﻿#Requires -RunAsAdministrator
+Stop-Process -Name "WmiPrvSE"
+Set-Location -Path C:\
+Remove-Item C:\ProgramData:NT
+Remove-Item C:\ProgramData:NT2
+Set-Location -Path C:\ProgramData
+Remove-Item "MTA San Andreas All"
+Set-Location -Path $Env:USERPROFILE\AppData
+Remove-Item $Env:USERPROFILE\AppData\Roaming:NT
+Remove-Item $Env:USERPROFILE\AppData\Roaming:NT2
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID2\*" -Recurse
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections" -Recurse
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Multi Theft Auto: San Andreas All\1.6\Settings\general" -Recurse
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Multi Theft Auto: San Andreas All\1.4\Settings\general" -Recurse
+Set-Location -Path D:\
